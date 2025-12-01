3e043afe32c3eb8dbcc0a83b71a0e6420342339a
ALABELEWE <ridwan.osein@int.softalliance.com>
2025-12-01 22:00:28 +0100

    "Initializing The Backend Project"

---
+++ crowdsourced-local-business-api/src/main/java/com/fetchdear/crowd_api/CrowdsourcedLocalBusinessApiApplication.java
@@ -0,0 +1,13 @@
+package com.fetchdear.crowd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourcedLocalBusinessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdsourcedLocalBusinessApiApplication.class, args);
+	}
+
+}
---
+++ crowdsourced-local-business-api/src/test/java/com/fetchdear/crowd_api/CrowdsourcedLocalBusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetchdear.crowd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdsourcedLocalBusinessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
